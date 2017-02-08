2ae57b98b5ca4a9beb8111432aa10bc6ce183c58
poornipras <spoorni.pr@gmail.com>
2017-02-08 10:44:27 +0530

    Initial commit

---
+++ app/src/main/java/com/pooja/batterylevel/MainActivity.java
@@ -0,0 +1,42 @@
+package com.pooja.batterylevel;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView textView;
+
+    private void getBatteryPercentage() {
+        BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+
+                int currentLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                int level = -1;
+                if (currentLevel >= 0 && scale > 0) {
+                    level = (currentLevel * 100) / scale;
+                }
+                textView.setText("Battery Level Remaining: " + level + "%");
+                context.unregisterReceiver(this);
+            }
+        };
+        IntentFilter batteryLevelFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryLevelReceiver, batteryLevelFilter);
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView= (TextView) findViewById(R.id.textView);
+        getBatteryPercentage();
+    }
+
+}
